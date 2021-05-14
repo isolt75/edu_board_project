634f50ef0a2c39b9bf25c28e5ddb25224d68fe7f
LYJ <dldudwls6376@naver.com>
2021-05-14 16:41:57 +0900

    MSG

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,12 +1,49 @@
 export const detailController = (req,res)=>{
-    res.render("screens/board/detail");
-}
+    const db = req.body.connection;
+
+    const sql = `
+    select   A.board_no                             AS no,
+             A.title                                AS title,
+             B.name                                 AS author,
+             '0'			                        AS hit,
+             date_format(A.created,'%Y-%m-%d')      AS created  
+   from 	board		A
+   join		emp			B
+     on 		A.author = B.m_no
+  where         A.board_no = ${req.query.pk}
+     `;
+ 
+     db.query(sql, (error, rows) => {
+         console.log(rows);
+         res.render(`screens/board/detail`);
+     });
+   
+   
+    };
 export const editController = (req,res)=>{
     res.render("screens/board/edit");
 }
 export const listController = (req,res)=>{
-    res.render("screens/board/list");
-}
+   const db = req.body.connection;
+
+   const sql = `
+   select   A.board_no  AS no,
+			A.title     AS title,
+            B.name      AS author,
+            '0'			AS hit,
+            date_format(A.created,'%Y-%m-%d')     AS created  
+  from 		board		A
+  join		emp			B
+    on 		A.author = B.m_no
+    `;
+
+    db.query(sql, (error, rows) => {
+        console.log(rows);
+        res.render("screens/board/list",{boardList : rows});
+    });
+   
+   
+};
 export const writeController = (req,res)=>{
     res.render("screens/board/write");
-}
+};
--- src/routers/boardRouter.js
+++ src/routers/boardRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { detailController, editController, listController, writeController } from "../controllers/boardController";
 
-
+import {dbConnectionHandler} from "../middleware/middleware";
 const boardRouter = express.Router();
 
 
-boardRouter.get("/list", listController);
+boardRouter.get("/list", dbConnectionHandler, listController);
 boardRouter.get("/write", writeController);
 boardRouter.get("/edit", editController);
-boardRouter.get("/detail", detailController);
+boardRouter.get("/detail", dbConnectionHandler, detailController);
 
 export default boardRouter;
